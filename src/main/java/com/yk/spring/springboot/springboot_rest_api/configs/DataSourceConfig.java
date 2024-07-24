d436fef844557e5e79ebd6061d72f10d921cb617
Yurii Kuleba <yurii.kuleba@nordstrom.com>
2024-07-24 18:50:58 +0300

    fifth commit

--- src/main/java/com/yk/spring/springboot/springboot_rest_api/configs/DataSourceConfig.java
+++ src/main/java/com/yk/spring/springboot/springboot_rest_api/configs/DataSourceConfig.java
@@ -1,22 +1,22 @@
-package com.yk.spring.springboot.springboot_rest_api.configs;
-
-
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
-
-@Configuration
-public class DataSourceConfig {
-
-    @Bean
-    public DataSource getDataSource() {
-        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.driverClassName("com.mysql.cj.jdbc.Driver");
-        dataSourceBuilder.url("jdbc:mysql://localhost:3306/example?useSSL=false&serverTimezone=UTC");
-        dataSourceBuilder.username("root");
-        dataSourceBuilder.password("");
-        return dataSourceBuilder.build();
-    }
-}
+//package com.yk.spring.springboot.springboot_rest_api.configs;
+//
+//
+//import org.springframework.boot.jdbc.DataSourceBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import javax.sql.DataSource;
+//
+//@Configuration
+//public class DataSourceConfig {
+//
+//    @Bean
+//    public DataSource getDataSource() {
+//        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+//        dataSourceBuilder.driverClassName("com.mysql.cj.jdbc.Driver");
+//        dataSourceBuilder.url("jdbc:mysql://localhost:3306/example?useSSL=false&serverTimezone=UTC");
+//        dataSourceBuilder.username("root");
+//        dataSourceBuilder.password("");
+//        return dataSourceBuilder.build();
+//    }
+//}
