b81ba5461648c7ecd1e3b5d5817fa2c56fb94389
Yurii Kuleba <yurii.kuleba@nordstrom.com>
2024-07-25 21:14:22 +0300

    ninth commit

--- src/main/java/com/yk/spring/springboot/springboot_rest_api/SpringbootRestApplication.java
+++ src/main/java/com/yk/spring/springboot/springboot_rest_api/SpringbootRestApplication.java
@@ -1,5 +1,6 @@
 package com.yk.spring.springboot.springboot_rest_api;
 
+import com.yk.spring.springboot.springboot_rest_api.configuration.DataSourceConfig;
 import com.yk.spring.springboot.springboot_rest_api.service.DataSourceService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,9 +10,9 @@ public class SpringbootRestApplication {
 
 	public static void main(String[] args) {
 
-		DataSourceService dataSourceService = new DataSourceService();
+		DataSourceConfig dataSourceConfig = new DataSourceConfig();
 
-		dataSourceService.readMyObjects();
+		dataSourceConfig.dataSource();
 
 		SpringApplication.run(SpringbootRestApplication.class, args);
 	}
